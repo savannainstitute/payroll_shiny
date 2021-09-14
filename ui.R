2b6140ca1886fc36a1d631fb830671231857db0b
shbrainard <scottbrainard@gmail.com>
2021-09-14 12:45:24 -0500

    ui

--- ui.R
+++ ui.R
@@ -1,12 +1,11 @@
-
 library(shiny)
 # UI
 
 
 fluidPage(
-  titlePanel("Savanna Institute Payroll JE Generator - test"),
+  titlePanel("Savanna Institute Payroll-Files Generator"),
   sidebarLayout(
-
+    
     sidebarPanel(
       fileInput('gusto', 'Select Gusto .csv file:',
                 accept = c("csv")),
@@ -14,30 +13,30 @@ fluidPage(
                 accept = c("csv")),
       fileInput('salary', 'Select Salary Allocation .csv file:',
                 accept = c("csv")),
-      fileInput('qsehra', 'Select QSEHRA Allocation .csv file:',
+      fileInput('qsehra', 'Select QSEHRA .csv file:',
                 accept = c("csv")),
-
+      
       dateInput("MINDATE", "Select start date (yyyy-mm-dd):", value = "2021-01-01"),
-      dateInput("MAXDATE", "Select end date (yyyy-mm-dd):",   value = "2021-01-31"),
-
+      dateInput("MAXDATE", "Select end date (yyyy-mm-dd):", value = "2021-07-30"),
+      
       numericInput("rows", "Rows to preview:", 10, min = 1),
-
-      textInput('archiveName',"Name of .zip archive:", value = "SI_payroll_JEs"),
-
-      div(style="display:inline-block;width:99%;text-align: center;", downloadButton('download', "Download .zip of payroll JE Excel files"))
-
-
-
+      
+      textInput('archiveName',"Name of .zip archive:", value = "payroll-files"),
+      
+      div(style="display:inline-block;width:99%;text-align: center;", downloadButton('download', "Download .zip archive of payroll Excel files"))
+      
+      
+      
     ),
-
+    
     mainPanel(
       # downloadButton("downloadData", "Download")),
       tabsetPanel(
-        tabPanel("Gusto file preview", tableOutput("gustoHead")),
+        tabPanel("Gusto file preview", tableOutput("gustoHead")), 
         tabPanel("TCH file preview", tableOutput("tchHead")),
         tabPanel("Salary Allocation file preview", tableOutput("salaryHead")),
-        tabPanel("QSEHRA Allocation file preview", tableOutput("qsehraHead"))
-        )
+        tabPanel("QSEHRA file preview", tableOutput("qsehraHead"))
       )
     )
   )
+)
